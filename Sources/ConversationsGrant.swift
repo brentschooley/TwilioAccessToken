3fc4d7b70626a51a277a8590cc4138c6ca774162
Brent Schooley <schooleybrent@gmail.com>
2016-10-14 15:16:54 -0400

    Added stub for ConversationsGrant

--- Sources/ConversationsGrant.swift
+++ Sources/ConversationsGrant.swift
@@ -1,3 +1,11 @@
 struct ConversationsGrant: Grant {
-  
+  var grantKey: String {
+    return "rtc"
+  }
+
+  var payload: [String:Any] {
+    var payloadValues: [String:String] = [:]
+
+    return payloadValues
+  }
 }
